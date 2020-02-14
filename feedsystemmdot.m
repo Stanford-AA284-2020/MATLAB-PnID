3b8305aa1cc14d7e7ada209506b49f0a5f8d91a4
Jeff-Robinson <jeffrobinson132@gmail.com>
2020-02-13 18:28:06 -0800

    starting tank blowdown calcs

---
+++ feedsystemmdot.m
@@ -0,0 +1,24 @@
+function [delta_mdot, mdot2, systable] = feedsystemmdot(systable, medium, Ptank, Ttank, A, mdot)
+% Propellant Feed System as function of mass flow
+%   Takes system definition table, working fluid, tank conditions, control
+%   orifice area, and mass flow rate.
+%   Calculates fluid conditions downstream of each component up to orifice,
+%   then calculates mass flow that would correspond to fluid conditions
+%   just upstream of orifice.
+%   Returns error between input mass flow rate and resultant orifice flow
+%   rate, the orifice flow rate itself, and the updated system table for
+%   the given input mass flow rate.
+%   CURRENTLY ONLY WORKS UP TO FLOW CONTROL ORIFICE
+    for i=1:length(systable.PartName)
+        if i==1
+            systable.P1(i) = Ptank;
+            systable.T1(i) = Ttank;
+        else
+            systable.P1(i) = systable.P2(i-1);
+            systable.T1(i) = systable.T2(i-1);
+        end
+        [systable.P2(i), systable.T2(i)] = valve(medium,mdot,systable.P1(i),systable.T1(i),systable.Cv(i));
+    end
+    mdot2 = chokedorifice(A,medium.gam,medium.Mw,systable.P2(end),systable.T2(end));
+    delta_mdot = mdot - mdot2;
+end
--- gastankblowdown.m
+++ gastankblowdown.m
@@ -16,57 +16,54 @@ FluidDatabase % Load in fluid properties
 % - If depressurization is complete, stop; otherwise repeat
 
 % Method here:
-% - Specify initial static pressure
-
-% - Calculate rate of discharge through orifice with static pressure
-% - Calculate pressure drop through valve, tubing, and regulator
+% Specify initial tank pressure
+% - Calculate initial tank fluid properties
+% Find mdot through system for given tank fluid properties
+% - Calculate pressure drop through valve, tubing, and regulator for input
+%   mass flow
 % - Calculate adiabatic temp change of gas due to pressure drop
-% - Recalculate rate of discharge with pressure drop
-% - Repeat until pressure drops & orifice mass flow match
-
-% - Specify time step & Calculate fluid mass discharged
-% - Perform isentropic expansion of gas in cylinder to find
-% - Repeat until P = Pa
+% - Recalculate rate of discharge with new P & T at orifice
+% - Iterate on mdot until input and orifice mass flows match
+% Calculate new tank properties after time step
+% - Specify time step
+% - Calculate fluid mass discharged
+% - Calculate new density in tank
+% - Calculate P & T from density, assuming isentropic expansion
+% - Repeat
 
 %% Initial tank parameters
-% V = 0.049;% m^3, Standard K cylinder volume is 49 L
+V = 0.049;% m^3, Standard K cylinder volume is 49 L
 A = pi*0.002^2;% Choked orifice area for 1100 psi upstream, 0.1081 kg/s
 Ptank0 = convpres(2000,"psi","Pa");% Pa
 Ttank0 = 298.15;% K
-% rho0 = PREoS(Methane,"rho",Ptank0,Ttank0);% kg/m^3
+rhotank0 = PREoS(Methane,"rho",Ptank0,Ttank0);% kg/m^3
 
 %% Initialize system as set of parts, flow coefficients, P and T values
-store = table('Size',[2 6],...
+systable = table('Size',[2 6],...
     'VariableTypes',{'string','double','double','double','double','double'},...
     'VariableNames',{'PartName','Cv','P1','P2','T1','T2'});
-store.PartName(1) = "HVMF"; store.Cv(1) = 0.69;
-store.PartName(2) = "RGMF"; store.Cv(2) = 0.3;
-store.P2(end) = Ptank0; store.T2(end) = Ttank0;% Initial orifice conditions
+systable.PartName(1) = "HVMF"; systable.Cv(1) = 0.69;
+systable.PartName(2) = "RGMF"; systable.Cv(2) = 0.3;
+systable.P2(end) = Ptank0; systable.T2(end) = Ttank0;% Initial orifice conditions
 
-%% Iterate to find dP through all parts, and mdot at orifice for given tank P & T
+%% Iterate as tank pressure drops
 Ptank = Ptank0;
 Ttank = Ttank0;% TODO: Add time-dependence & tank draining
-% System as function of mass flow
-system = @(mdot) systemmdot(store, mdot, Methane, Ptank, Ttank, A);
+rhotank = rhotank0;
+tstep = 0.1;% sec
 
-[mdotll, mdotul] = bracket_sign_change(system,0,0.1)
-[mdotll, mdotul] = bisection(system,mdotll,mdotul)
-mdot_opt = mean([mdotll,mdotul])
+%% Iterate to find dP through all parts, and mdot at orifice for given tank P & T
+% Create system as function exclusively of mdot, to find actual mdot
+% through orifice based on valve dP between tank and orifice. See
+system = @(mdot) feedsystemmdot(systable, Methane, Ptank, Ttank, A, mdot);
+% Find a bracket of mdot values for which the flow rate delta crosses zero
+[mdotll, mdotul] = bracket_sign_change(system,0,0.1);
+% Find the "root" of the system via bisection to find mass flow input that
+% corresponds to orifice mass flow.
+[mdotll, mdotul] = bisection(system,mdotll,mdotul);
+mdot_opt = mean([mdotll,mdotul]);
+% Get updated system table for 'optimal' mass flow rate
+[dmdot, mdot, systable] = feedsystemmdot(systable, mdot_opt, Methane, Ptank, Ttank, A)
 
-[dmdot, mdot2, store] = systemmdot(store, mdot_opt, Methane, Ptank, Ttank, A)
-
-% System as function of mass flow
-function [delta_mdot, mdot2, store] = systemmdot(store,mdot,medium,Ptank,Ttank,A)
-    for i=1:length(store.PartName)
-        if i==1
-            store.P1(i) = Ptank;
-            store.T1(i) = Ttank;
-        else
-            store.P1(i) = store.P2(i-1);
-            store.T1(i) = store.T2(i-1);
-        end
-        [store.P2(i), store.T2(i)] = valve(medium,mdot,store.P1(i),store.T1(i),store.Cv(i));
-    end
-    mdot2 = chokedorifice(A,medium.gam,medium.Mw,store.P2(end),store.T2(end));
-    delta_mdot = mdot - mdot2;
-end
+%% New Tank Properties
+rhotank = rhotank
